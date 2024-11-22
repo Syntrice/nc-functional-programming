96dff5ca991554961a65591209721a1a2609367f
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-22 14:17:19 +0000

    Add functionality for working with peoples' email addresses to Exercise002

--- Functional+programming/Functional+programming/Exercises002.cs
+++ Functional+programming/Functional+programming/Exercises002.cs
@@ -1,17 +1,56 @@
-﻿using System.Threading.Channels;
-
-namespace FunctionalProgramming
+﻿namespace FunctionalProgramming
 {
     public class Exercises002
     {
-        public static Func<List<string>> GetCoolPeople = () => new List<string> {"rich.neat@boardgamer.com",
-                                                                                    "poppy.mcdonnell@irishdancer.com",
-                                                                                    "neil.hughes@walkingoncustard.com",
-                                                                                    "alice.yang@midfielder.com",
-                                                                                    "pippa.austin@musician.com"};
-        public static Action GetUsernames = () => {
+        public static Func<List<string>> GetCoolPeople = () => new List<string> 
+        {
+            "rich.neat@boardgamer.com",
+            "poppy.mcdonnell@irishdancer.com",
+            "neil.hughes@walkingoncustard.com",
+            "alice.yang@midfielder.com",                                            
+            "pippa.austin@musician.com"
+        };
+
+        public static Action GetUsernames = () =>
+        {
             var namesList = GetCoolPeople();
             namesList.ForEach(name => Console.WriteLine(name.Split('@')[0]));
         };
+
+        public static Action<List<string>> PrintCoolPeople = list => list.ForEach(name =>
+        {
+            Console.WriteLine(name);
+        });
+
+        public static Action<List<int>> SquaredNums = list =>
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i] * list[i];
+            }
+        };
+
+        public static Action<List<int>> PrintNums = list => list.ForEach(Console.WriteLine);
+        public static Action<List<int>> PrintSquareNums = list =>
+        {
+            SquaredNums(list);
+            PrintNums(list);
+        };
+
+        public static Dictionary<string, List<string>> FilterEmails(List<string> list)
+        {
+            Dictionary<string, List<string>> emailsByExtension = new Dictionary<string, List<string>>();
+
+            String[] filterDomains = [".co.uk", ".com"];
+
+            foreach (string domain in filterDomains)
+            {
+                emailsByExtension.Add(domain, list.Where(email => email.Contains(domain)).ToList());
+            }
+
+            emailsByExtension.Add("invalid", list.Where(email => !filterDomains.Any(extension => email.Contains(extension))).ToList());
+
+            return emailsByExtension;
+        }
     }
 }
--- Functional+programming/Functional+programming/Program.cs
+++ Functional+programming/Functional+programming/Program.cs
@@ -23,7 +23,35 @@
 
             //Console.WriteLine(Exercises001.SumIndices("start", "pale"));
 
-            Exercises002.GetUsernames();
+            ////Exercises002.GetUsernames();
+
+            //var names = Exercises002.GetCoolPeople();
+            //Exercises002.PrintCoolPeople(names);
+
+            //List<int> numList = new List<int> { 5, 8, 3, 6, 67 };
+            //Exercises002.PrintSquareNums(numList);
+
+            List<string> emailList = new List<string>
+            {
+                "alice.yang@northcoders.com",
+                "richard.neat@northcoders.com",
+                "mario@plumbing.it",
+                "link@hyrule.co.uk",
+                "shrek@duloc.com",
+                "neil.hughes@walkingoncustard.com",
+                "csharp@microsoft.cs",
+                "ziggy@spidersfrommars.co.uk",
+                "lemmy@motorhead.co,uk",
+                "me@myhouse.sleep"
+            };
+
+            var filteredEmails = Exercises002.FilterEmails(emailList);
+
+            foreach (var item in filteredEmails)
+            {
+                Console.WriteLine(item.Key);
+                item.Value.ForEach(Console.WriteLine);
+            }
 
         }
     }
