d2c4fe9ebff2fe8fdfdcc3dc926379a18c742c15
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-22 11:49:00 +0000

    Add GrammerCheck, SumIndices, and CheckValidEmail functionality

--- Functional+programming/Functional+programming/Exercises001.cs
+++ Functional+programming/Functional+programming/Exercises001.cs
@@ -6,5 +6,30 @@
 
     public static Func<int, int> AddTen = num => num + 10;
 
+    public static Predicate<string> GrammarCheck = str => str[0] == 'A' && str[^1] == '!';
+
+    public static Func<string, string, int> SumIndices = (word1, word2) =>
+    {
+        return word1.ToLower().IndexOf('a') + word2.ToLower().IndexOf('e');
+    };
+
+    public static string CheckValidEmail(string email)
+    {
+
+        Predicate<string> isNorthCoders = email => email.Split('@')[1] == "northcoders.co.uk";
+        Predicate<string> usernameFiveOrMore = email => email.Split('@')[0].Length >= 5;
+
+        if (isNorthCoders(email) && usernameFiveOrMore(email))
+        {
+            return "Email domain and user valid, please continue";
+        }
+        else
+        {
+            return "Email domain and user name invalid, please check your input";
+        }
+
+
+    }
+
 
 }
