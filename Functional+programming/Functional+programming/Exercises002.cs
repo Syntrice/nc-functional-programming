d82d2b037cb627f509c10371497dba841cafd17c
termgiles <Thomas.giles2222@gmail.com>
2024-11-22 12:32:03 +0000

    add get username delegate

---
+++ Functional+programming/Functional+programming/Exercises002.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+
+namespace FunctionalProgramming
+{
+    public class Exercises002
+    {
+        public static Func<List<string>> GetCoolPeople = () => new List<string> {"rich.neat@boardgamer.com",
+                                                                                    "poppy.mcdonnell@irishdancer.com",
+                                                                                    "neil.hughes@walkingoncustard.com",
+                                                                                    "alice.yang@midfielder.com",
+                                                                                    "pippa.austin@musician.com"};
+        public static Action GetUsernames = () => {
+            var namesList = GetCoolPeople();
+            namesList.ForEach(name => Console.WriteLine(name.Split('@')[0]));
+        };
+    }
+}
--- Functional+programming/Functional+programming/Program.cs
+++ Functional+programming/Functional+programming/Program.cs
@@ -1,29 +1,29 @@
 ﻿namespace FunctionalProgramming
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
+            //List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
 
-            List<int> numbersAddTen = new List<int>();
-            numbers.ForEach((x) => { 
-                var addedTen = Exercises001.AddTen(x);
-                numbersAddTen.Add(addedTen);
-            });           //declaritive
-
-
-            for (int i = 0; i < numbersAddTen.Count; i++)             //imperative
-            {
-                Console.WriteLine(numbersAddTen[i]);
-            }
-
-            List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
-            words.ForEach(word => Console.WriteLine(Exercises001.GrammarCheck(word)));
-
-            Console.WriteLine(Exercises001.SumIndices("start", "pale"));
+            //List<int> numbersAddTen = new List<int>();
+            //numbers.ForEach((x) => { 
+            //    var addedTen = Exercises001.AddTen(x);
+            //    numbersAddTen.Add(addedTen);
+            //});           //declaritive
 
 
+            //for (int i = 0; i < numbersAddTen.Count; i++)             //imperative
+            //{
+            //    Console.WriteLine(numbersAddTen[i]);
+            //}
+
+            //List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
+            //words.ForEach(word => Console.WriteLine(Exercises001.GrammarCheck(word)));
+
+            //Console.WriteLine(Exercises001.SumIndices("start", "pale"));
+
+            Exercises002.GetUsernames();
 
         }
     }
