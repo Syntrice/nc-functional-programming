aae67f27558dda75bd9af9512eb74a6eb6327958
termgiles <Thomas.giles2222@gmail.com>
2024-11-22 11:06:16 +0000

    add tasks 1 -3, Funcs for adding one, squaring, and adding ten

---
+++ Functional+programming/Functional+programming/Exercises001.cs
@@ -0,0 +1,10 @@
+﻿public class Exercises001
+{
+    public static Func<int, int> AddOne = num => num + 1;
+
+    public static Func<int, int> SquareIt = num => num * num;
+
+    public static Func<int, int> AddTen = num => num + 10;
+
+
+}
--- Functional+programming/Functional+programming/Program.cs
+++ Functional+programming/Functional+programming/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
+
+            List<int> numbersAddTen = numbers.Select(Exercises001.AddTen).ToList();
+            numbersAddTen.ForEach(Console.WriteLine);           //declaritive
+
+            for (int i = 0; i < numbersAddTen.Count; i++)             //imperative
+            {
+                Console.WriteLine(numbersAddTen[i]);
+            }
+
         }
     }
 }
